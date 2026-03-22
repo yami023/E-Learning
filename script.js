b0551adbd3b13c310222799d237388dd98a574b1
yami023 <lifearmyfor062@gmail.com>
2026-03-22 14:09:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function login(){
+alert("Login Success");
+}
+
+function register(){
+alert("Registered");
+}
+
+function openM(){
+alert("Module Opened");
+}
+
+function quiz(){
+alert("Quiz Started");
+}
+
+function feedback(){
+alert("Feedback sent");
+}
+
+function chat(){
+alert("Chat opened");
+}
+
+function admin(){
+alert("Admin page");
+}
